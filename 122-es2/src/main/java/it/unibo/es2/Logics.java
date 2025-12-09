a4cec5af33b99c70307fc59a0a459b0df9f57251
Samuele <samuele.bianchini4@studio.unibo.it>
2025-12-09 11:24:09 +0100

    Start exercise 122

---
+++ 122-es2/src/main/java/it/unibo/es2/Logics.java
@@ -0,0 +1,14 @@
+package it.unibo.es2;
+
+/**
+
+*/
+public interface Logics {
+
+    /**
+        This method updates the button when hit
+
+        @param 
+    */
+    public char hit(final Pair<Integer, Integer> pair);
+}
---
+++ 122-es2/src/main/java/it/unibo/es2/LogicsImpl.java
@@ -0,0 +1,15 @@
+package it.unibo.es2;
+
+import java.util.ArrayList;
+
+public class LogicsImpl implements Logics{
+
+
+    /**
+     * Constructor.
+     *
+     * @param size the size of the logics
+     */
+    public LogicsImpl(final int size) {
+    }
+}
